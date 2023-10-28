39a2894431aca43f43b6df20f6bb4e2066f7698b
ahmedyazeed86 <ahmedyazeed86@gmail.com>
2023-10-28 12:19:07 -0400

    first commit

---
+++ src/test/java/class01/priority.java
@@ -0,0 +1,22 @@
+package class01;
+
+import org.testng.annotations.Test;
+
+public class priority {
+//if I will not use priority keyword then it will execute based on the
+// alphabetical order of the methods names
+    //another note that priority = 0 by default is the highest priority
+    @Test(priority = 2)
+    public void Atest(){
+        System.out.println("i am TestA");
+    }
+
+    @Test(priority = 1)
+    public void Ctest(){
+        System.out.println("i am TestC");
+    }
+    @Test(priority = 3)
+    public void Btest(){
+        System.out.println("i am TestB");
+    }
+}
